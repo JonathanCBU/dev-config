2930fc17cac97df8928938b575753ed607ee53ba
Jonathan Cook <jcookbme@gmail.com>
2025-12-18 09:08:22 -0500

    Add new cholorscheme

---
+++ nvim/lua/custom/plugins/tokyodark.lua
@@ -0,0 +1,11 @@
+-- tokyodark colorscheme
+return {
+	"tiagovla/tokyodark.nvim",
+	opts = {
+		-- custom options here
+	},
+	config = function(_, opts)
+		require("tokyodark").setup(opts) -- calling setup is optional
+		vim.cmd([[colorscheme tokyodark]])
+	end,
+}
