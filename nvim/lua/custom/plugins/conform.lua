2f5cdc1ef7e3b2887229da57150d46b1025f4021
Jonathan Cook <jcookbme@gmail.com>
2026-01-14 14:38:29 -0500

    Remove auto format

--- nvim/lua/custom/plugins/conform.lua
+++ nvim/lua/custom/plugins/conform.lua
@@ -16,17 +16,7 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true }
-			local lsp_format_opt
-			if disable_filetypes[vim.bo[bufnr].filetype] then
-				lsp_format_opt = "never"
-			else
-				lsp_format_opt = "fallback"
-			end
-			return {
-				timeout_ms = 500,
-				lsp_format = lsp_format_opt,
-			}
+			return false
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
@@ -35,10 +25,10 @@ return {
 
 			markdown = { "prettierd", "prettier" },
 
-			-- javascript = { "prettierd", "prettier" },
-			-- javascriptreact = { "prettierd", "prettier" },
-			-- typescript = { "prettierd", "prettier" },
-			-- typescriptreact = { "prettierd", "prettier" },
+			javascript = { "prettierd", "prettier" },
+			javascriptreact = { "prettierd", "prettier" },
+			typescript = { "prettierd", "prettier" },
+			typescriptreact = { "prettierd", "prettier" },
 
 			sh = { "shfmt" },
 			zsh = { "shfmt" },
